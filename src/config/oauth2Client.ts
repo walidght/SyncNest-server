4112ec41a6fd4edddaee418ef5856415e4df5e8e
Walid Ghenaiet <77700354+walidght@users.noreply.github.com>
2024-05-09 01:46:12 +0200

    now saving/getting user info in/from db

--- src/config/oauth2Client.ts
+++ src/config/oauth2Client.ts
@@ -1,9 +1,9 @@
 import { google } from 'googleapis';
 
 const CLIENT_ID =
-    '492113110386-c3n2040dsegoqup3eog45gqjn83oo696.apps.googleusercontent.com';
-const CLIENT_SECRET = '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';
-const REDIRECT_URI = 'http://localhost:5173';
+    'CLIENT_ID';
+const CLIENT_SECRET = 'CLIENT_SECRET';
+const REDIRECT_URI = 'REDIRECT_URI';
 
 export const oauth2Client = new google.auth.OAuth2(
     CLIENT_ID,
